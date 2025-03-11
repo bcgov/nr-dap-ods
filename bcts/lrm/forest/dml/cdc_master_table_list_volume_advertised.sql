489b47fe29e27e62f2ef4a28714facc31b5e4894
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-03-11 09:30:36 -0700

    Added LRM tables replication and initial version of transformation script

---
+++ bcts/lrm/forest/dml/cdc_master_table_list_volume_advertised.sql
@@ -0,0 +1,20 @@
+INSERT INTO ods_data_management.cdc_master_table_list 
+    VALUES (
+        NULL,
+        'lrm',
+        NULL,               
+        'forestview',
+        'v_cp',               
+        'lrm_replication',
+        'v_cp',
+        'Y',
+        NULL,
+        NULL,
+        NULL,
+        'Y',
+        1,
+        'N',
+        'N',
+        NULL,
+        'Oracle'
+    );
