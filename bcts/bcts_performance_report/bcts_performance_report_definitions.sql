0dab001dfbfdecb144d2e4385b3efd38b37ac0aa
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-06-24 13:14:01 -0700

    Updated BCTS Performance Report definitions

--- bcts/bcts_performance_report/bcts_performance_report_definitions.sql
+++ bcts/bcts_performance_report/bcts_performance_report_definitions.sql
@@ -14,8 +14,8 @@ INSERT INTO bcts_reporting.bcts_performance_report_definitions (Term, Definition
 ('Currently in Market', 'Open to bidders on BC Bid at the end of the reporting period. Includes reauctions.'),
 ('Licence Issued', 'Volume of timber sold in issued licences. Issued licences may be first auctioned in a previous fiscal and not appear in this report’s Auctioned volume.'),
 ('Not Awarded', 'Volume of timber in auctioned licences that did not have a successful bidder. Includes reauctioned licences.'),
-('Licence Issued Target: Value Added (VA)', 'Cumulative target volume of timber sold in Value Added issued licences for the end of the current quarter, updated quarterly. Based on 10% of Rationalized Apportionment, proportionate to quarterly sales target.'),
-('Auctioned: Value Added (VA)', 'New-to-market Value Added auctions. Includes issued licences, awarded licences that are not yet issued, and licences not awarded. Excludes reauctions.'),
-('Currently in Market: Value Added (VA)', 'Value Added volume open to bidders on BC Bid at the end of the reporting period. Includes reauctions.'),
-('Licence Issued: Value Added (VA)', 'Volume of timber sold in Value Added issued licences. Issued licences may be first auctioned in a previous fiscal and not appear in this report’s Auctioned: Value Added volume.'),
-('Not Awarded: Value Added (VA)', 'No successful bidder for licence advertised as Value Added; to be reauctioned.');
+('Auctioned Target: Value Added', 'Cumulative target volume of timber volume auctioned, targeted at the value-added sector for the end of the current quarter, updated quarterly. Based on 20% of the Rationalized Apportionment of 6,467,328 m³, proportionate to quarterly target.'),
+('Auctioned: Value Added', 'New-to-market Value Added auctions. Includes issued licences, awarded licences that are not yet issued, and licences not awarded. Excludes reauctions.'),
+('Currently in Market: Value Added', 'Value Added volume open to bidders on BC Bid at the end of the reporting period. Includes reauctions.'),
+('Licence Issued: Value Added', 'Volume of timber sold in Value Added issued licences. Issued licences may be first auctioned in a previous fiscal and not appear in this report’s Auctioned: Value Added volume.'),
+('Not Awarded: Value Added', 'No successful bidder for licence advertised as Value Added; to be reauctioned.');
