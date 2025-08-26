d432e53bbe8f2758ecbbf35728b1c9f2e8529628
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-08-26 13:04:45 -0700

    Added more logs

--- shared/bcts_reports_etl/bcts_validate_write_off_forms_transformation.py
+++ shared/bcts_reports_etl/bcts_validate_write_off_forms_transformation.py
@@ -319,7 +319,7 @@ def validate_write_off_forms():
             df_summary['form'] = form.split('/')[-1]
             df_summary['expected_value'] = df_summary['expected_value'].apply(normalize)
             df_summary['filled_value']   = df_summary['filled_value'].apply(normalize)
-            df_summary['match'] = df_summary['expected_norm'] == df_summary['filled_norm']
+            df_summary['match'] = df_summary['expected_value'] == df_summary['filled_value']
             df_summary['validated_date'] = datetime.now().date()
             load_into_ods(df_summary)
             logging.info(f"Form {form} validated successfully.")
