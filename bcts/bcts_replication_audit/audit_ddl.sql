3252402af7e9a9a8529bfaaed8cc804181020d41
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-09-09 12:19:25 -0700

    Updated DML for results replication

--- bcts/bcts_replication_audit/audit_ddl.sql
+++ bcts/bcts_replication_audit/audit_ddl.sql
@@ -1,5 +1,12 @@
-create or replace view bcts_staging.replication_errors as
+
+create or replace view bcts_staging.replication_errors_bcts as
 select * from ods_data_management.audit_batch_status
 where application_name in ('lrm', 'bctsadmin', 'mof-client') 
-and object_execution_status='failed' 
-and batch_run_date=(select max(batch_run_date) from ods_data_management.audit_batch_status where application_name in ('lrm', 'bctsadmin', 'mof-client'));
+and batch_run_date::date = CURRENT_DATE
+
+union all
+
+select * from ods_data_management.audit_batch_status
+where application_name = 'FTA' and batch_run_date::date = CURRENT_DATE
+and lower(object_name) in ('tenure_term', 'prov_forest_use', 'tenure_file_status_code', 'tfl_number_code', 'sale_method_code', 'org_unit', 'sb_category_code', 'harvest_sale', 'tsa_number_code', 'forest_file_client', 'TIMBER_MARK')
+order by object_execution_status;
