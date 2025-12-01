ac9cbf4c57adb018f8c4b94380c9e8eacb73c0c8
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-12-01 09:48:08 -0800

    Added DDL and DML for CLIENT table, file_client_type_code

---
+++ bcts/client/ddl/licence_transfer_report_ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE mofclient_replication.file_client_type_code (
+    file_client_type_code VARCHAR(2),
+    description VARCHAR(120),
+    effective_date TIMESTAMP,
+    expiry_date TIMESTAMP,
+    update_timestamp TIMESTAMP
+);
---
+++ bcts/client/dml/licence_transfer_report_dml.sql
@@ -0,0 +1,20 @@
+INSERT INTO ods_data_management.cdc_master_table_list 
+VALUES (
+    NULL,
+    'mof-client',
+    NULL,               
+    'the',
+    'file_client_type_code',               
+    'mofclient_replication',
+    'file_client_type_code',
+    'Y',
+    NULL,
+    NULL,
+    NULL,
+    'Y',
+    1,
+    'N',
+    'N',
+    NULL,
+    'Oracle'
+);
