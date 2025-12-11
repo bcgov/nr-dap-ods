aaa5ba6d137159ede2cd529b3eef2d32e26501a5
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-12-11 10:40:23 -0800

    Added logs for debugging

--- shared/bcts_reports_etl/bcts_validate_write_off_forms_transformation.py
+++ shared/bcts_reports_etl/bcts_validate_write_off_forms_transformation.py
@@ -201,7 +201,7 @@ def fetch_from_ods(ubi):
     sql_statement = \
         f"""
         with min_date as
-            (select  min(TO_DATE(activity_date, 'YY-MM-DD')) as earliest_date
+            (select  min(TO_DATE(activity_date, 'RR-MM-DD')) as earliest_date
             from FORESTVIEW.v_block_activity_all
             where ubi = '{ubi}'
             and actt_key_ind in ('DVC', 'DVS', 'DR')
