8ceb5c737574aead7e92ea9f3cd8b078d56f0278
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-12-15 10:10:33 -0800

    Updated replication error view

--- bcts/bcts_replication_audit/audit_ddl.sql
+++ bcts/bcts_replication_audit/audit_ddl.sql
@@ -1,12 +1,12 @@
-
 create or replace view bcts_staging.replication_errors_bcts as
 select * from ods_data_management.audit_batch_status
-where application_name in ('lrm', 'bctsadmin', 'mof-client', 'results') 
+where application_name in ('lrm', 'lrm2', 'bctsadmin', 'mof-client', 'results') 
 and batch_run_date::date = CURRENT_DATE
 
 union all
 
 select * from ods_data_management.audit_batch_status
 where application_name = 'FTA' and batch_run_date::date = CURRENT_DATE
-and lower(object_name) in ('tenure_term', 'prov_forest_use', 'tenure_file_status_code', 'tfl_number_code', 'sale_method_code', 'org_unit', 'sb_category_code', 'harvest_sale', 'tsa_number_code', 'forest_file_client', 'TIMBER_MARK')
+and lower(object_name) in ('tenure_term', 'prov_forest_use', 'tenure_file_status_code', 'tfl_number_code', 'sale_method_code', 'org_unit', 'sb_category_code', 'harvest_sale', 'tsa_number_code', 'forest_file_client', 'timber_mark', 'mgmt_unit_type_code')
 order by object_execution_status;
+
