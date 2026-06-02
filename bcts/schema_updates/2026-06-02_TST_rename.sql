539c5b31c0a526bc3548bfb2d94cc6de5c31ecfd
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2026-06-02 15:21:44 -0700

    Added DML to update TST business area name

---
+++ bcts/schema_updates/2026-06-02_TST_rename.sql
@@ -0,0 +1,155 @@
+UPDATE bcts_reporting.annual_developed_volume_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.annual_development_ready_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.currently_in_market_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+-- UPDATE bcts_reporting.forestview_v_species_composition_hist
+-- SET business_area = 'Seaward-tlasta (TST)'
+-- WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.licence_issued_advertised_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.licence_issued_with_unbilled_volume_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+-- UPDATE bcts_reporting.licence_sold_to_out_of_province_registrants_hist
+-- SET business_area = 'Seaward-tlasta (TST)'
+-- WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.licence_transfer_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.roads_constructed_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.roads_deactivated_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.roads_planned_deactivation_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.roads_transferred_in_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.roads_transferred_out_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.timber_inventory_development_in_progress_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.timber_inventory_ready_to_develop_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.timber_inventory_ready_to_sell_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.tsl_summary_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.volume_advertised_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_reporting.weighted_sale_term_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.annual_developed_volume_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.annual_development_ready_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.bcts_sales_targets_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.currently_in_market_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.licence_issued_advertised_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.licence_issued_with_unbilled_volume_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+-- UPDATE bcts_staging.licence_sold_to_out_of_province_registrants_hist
+-- SET business_area = 'Seaward-tlasta (TST)'
+-- WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.licence_transfer_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.roads_constructed_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.roads_deactivated_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.roads_planned_deactivation_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.roads_transferred_in_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.roads_transferred_out_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.silviliability_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.timber_inventory_development_in_progress_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.timber_inventory_ready_to_develop_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.timber_inventory_ready_to_sell_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.tsl_summary_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.volume_advertised_main_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
+
+UPDATE bcts_staging.weighted_sale_term_hist
+SET business_area = 'Seaward-tlasta (TST)'
+WHERE business_area = 'Seaward-Tlasta (TST)';
