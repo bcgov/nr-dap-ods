513d66d21bc649ef5f9c77e6205351e92a823593
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2026-06-19 09:39:01 -0700

    Added new columns to LRM tables

--- bcts/schema_updates/2026-06-18-LRM-schema-changes.sql
+++ bcts/schema_updates/2026-06-18-LRM-schema-changes.sql
@@ -3,3 +3,6 @@ alter table lrm_replication.v_block
 
 alter table lrm_replication.silviculture_stratum
     add column if not exists dep_ind text;
+
+alter table lrm_replication.v_block
+    add column if not exists cutb_rc_risk_comments text;
